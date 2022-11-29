1517753dfde4b85b0b0de98c3e51e0f7401d1d43
tomas <tomasgonz@gmail.com>
2022-11-29 20:35:25 +0000

    Finalize third person to first person converted

--- pages/First to third person converter.py
+++ pages/First to third person converter.py
@@ -6,3 +6,24 @@ import os
 openai.organization = "org-BKMBE8WdTAJKgRdZfV9tMxBt"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.Model.list()
+
+prompt = st.text_area("Enter your input here")
+
+model = st.sidebar.selectbox("Select engine", ['text-davinci-003', 'ml.p3.2x.large'])
+
+temperature = st.sidebar.slider("Temperature", 0.0, 1.0, 0.7, 0.1)
+response_length = st.sidebar.slider("Response length", 0, 1000, 200, 10)
+
+if st.button("Run"):
+    response = openai.Completion.create(
+        model = model,
+        prompt = prompt,        
+        max_tokens = response_length,
+        temperature=temperature,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+
+    st.write(response['choices'][0]['text'])
+
